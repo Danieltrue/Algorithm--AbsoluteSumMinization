cb45d3bd08cf6740d84588360082340b66abeaff
DanielTrue <danielokezie75@gmail.com>
2022-04-19 15:31:53 +0100

    Finished

--- index.ts
+++ index.ts
@@ -4,5 +4,5 @@ const AbsoluteSumMinization = (sum: number[]): any => {
   return isEven ? sum[sum.length / 2 - 1] : sum[Math.floor(sum.length / 2)];
 };
 
-console.log(AbsoluteSumMinization([1, 2, 3, 4, 5, 6]));
-console.log(AbsoluteSumMinization([1, 2, 3, 4, 5, 6, 7]));
+console.log(AbsoluteSumMinization([1, 2, 3, 4, 5, 6])); // 3
+console.log(AbsoluteSumMinization([1, 2, 3, 4, 5, 6, 7])); // 4
